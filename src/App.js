0c66cd5bb548ad8bcab909600526be8dd753b7a3
하늘 <153697652+Leedo02@users.noreply.github.com>
2024-06-11 13:28:15 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -295,7 +295,7 @@ function App() {
           className='active'
         >전체</button>
         <button
-          onClick={() => clickYearBtn('2020년')}
+          onClic={() => clickYearBtn('2020년')}
           className='active'
         >2020년</button>
         <button
